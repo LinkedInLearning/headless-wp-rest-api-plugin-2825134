ebaba68f9326668883c4cc412fa4cac014943135
Morten Rand-Hendriksen <morten@pinkandyellow.com>
2020-02-07 16:37:40 -0800

    Add changes

--- taskbook/taskbook.php
+++ taskbook/taskbook.php
@@ -55,3 +55,28 @@ require_once plugin_dir_path( __FILE__ ) . 'includes/status.php';
  * Register CMB2 metaboxes and fields.
  */
 require_once plugin_dir_path( __FILE__ ) . 'includes/CMB2-functions.php';
+
+/**
+ * Grant access to tasks only to authenticated users
+ * with either administrator or task logger roles.
+ */
+add_action( 'pre_get_posts', 'taskbook_grant_access' );
+
+function taskbook_grant_access( $query ) {
+    // Make sure the query contains a post_type query_var,
+    // otherwise it's definitely not a request for Task(s):
+    if ( isset($query->query_vars['post_type']) ) {
+        // Check if the request is for the Task post type…
+        if ( $query->query_vars['post_type'] == 'task' ) {
+            // … and that this is a REST request:
+            if ( defined( 'REST_REQUEST' ) && REST_REQUEST ) {
+                // … and the current user is a task logger"
+                if ( current_user_can( 'task_logger' ) ) {
+                    // … the user can see only their own private tasks:
+                    $query->set( 'post_status', 'private' );
+                    $query->set( 'author', get_current_user_id() );
+                }
+            }
+        }
+    }
+}
